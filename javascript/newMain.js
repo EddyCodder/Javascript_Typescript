140cd18ed653320a0b7390e65753ef35a28d1a47
EddyCodder <ed.durand.obando@gmail.com>
2025-05-05 18:10:58 -0500

    Agregando función que retorna bool si es mayor de edad

--- javascript/newMain.js
+++ javascript/newMain.js
@@ -12,7 +12,13 @@ class Persona {
         return `Hola, soy ${this._nombre}, tengo ${this._edad} años y vivo en ${this._ciudad}`
     }
 
+    esMayor() {
+        return this._edad >= 18 // Si es mayor devuelve su edad
+    }
+
 }
 
 const persona1 = new Persona("Eduardo", 21, "Arequipa");
 console.log(persona1.presentarse());
+
+console.log(persona1.esMayor());
