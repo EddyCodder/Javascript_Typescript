47770dd6c2e64be16627c2b93fb36e3a47388626
EddyCodder <ed.durand.obando@gmail.com>
2025-05-05 19:14:40 -0500

    Uso de sort

--- javascript/newMain.js
+++ javascript/newMain.js
@@ -40,8 +40,8 @@ const numeros = [1, 2, 3, 4]
 const dobles = numeros.map(n => n * 2) 
 const dobles2 = numeros.forEach(n => n * 2) // error - undefined
 
-console.log(dobles)
-console.log(dobles2)
+console.log("Map:", dobles)
+console.log("ForEach:", dobles2)
 
 
 // filter - permite filtrar por una condición al arreglo
@@ -54,7 +54,7 @@ console.log(`Filter (<3): ${menores_a_3}`)
 
 
 // find - encuentra un valor de acuerdo a una condición
-const solo4 = numeros.find(n => n == 4);
+const solo4 = numeros.find(n => n === 4); // === iguala las referencias
 console.log("Find:", solo4); // 4
 
 
@@ -66,3 +66,27 @@ console.log("Replace:", nuevoSaludo); // "Hola Eduardo"
 // repeat (string)
 const eco = "Hey! ".repeat(3); // retorna 3 veces la cadena
 console.log("Repeat:", eco); // "Hey! Hey! Hey! "
+
+
+// reduce - (como una prensa) aplica una función reductora para cada valor del arreglo
+// función acumuladora
+const acumular = (acum, n) => acum + n 
+const suma = numeros.reduce(acumular, 0); // (funcion acumuladora, valor inicial)
+// 0 es opcional siempre y cuando el array no esté vacío
+
+console.log("Reduce:", suma); // 10
+
+// sort
+// ordena los elementos de un arreglo, lo hace como transformando a cadenas de texto
+// y comparando el valor de unicode
+const desordenados1 = [3, 1, 4, 2];
+const desordenados2 = [3, 1, 4, 2];
+
+const ordenados_asc = desordenados1.sort((a, b) => a - b); // modifica el arreglo original
+const ordenados_desc = desordenados2.sort( (a,b) => b - a);
+
+console.log("Sort (ascendente):", ordenados_asc); // [1, 2, 3, 4]
+console.log("Sort (descendente):", ordenados_desc); // [4, 3, 2, 1]
+
+console.log("Sort (desordenados1):", desordenados1); // [3, 1, 4, 2]
+console.log("Sort (desordenados2):", desordenados2); // [3, 1, 4, 2]
